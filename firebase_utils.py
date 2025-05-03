c6ad164bbec4a418e592f79e39ec83e2faefde9a
chincode007 <cjpai2003@gmail.com>
2025-05-03 16:42:05 +0530

    fifth  commit

--- firebase_utils.py
+++ firebase_utils.py
@@ -2,31 +2,25 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 from datetime import datetime
+import streamlit as st
 
-# Check if running on Streamlit Cloud or locally
-if "GOOGLE_APPLICATION_CREDENTIALS" in os.environ:
-    # When deployed on Streamlit Cloud (or any environment with the correct environment variable)
-    cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-else:
-    # When running locally, use the local path to the firebase credentials file
-    cred_path = os.path.join(os.getcwd(), "firebase-credentials.json")
+# Load Firebase credentials from Streamlit secrets
+cred_json = st.secrets["firebase-credentials"]
 
+# Initialize Firebase if not already initialized
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_path)
+    cred = credentials.Certificate(cred_json)
     firebase_admin.initialize_app(cred)
 
+# Firestore client
 db = firestore.client()
 
 def store_result(edited):
-    try:
-        # Get the current date and time
-        current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-        # Add data to Firestore
-        doc_ref = db.collection("weights").add({
-            "weight": edited,
-            "date": current_date
-        })
-        print(f"Document successfully added with ID: {doc_ref.id}")
-    except Exception as e:
-        print("Error adding document to Firestore:", e)
+    # Get the current date and time
+    current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Add data to Firestore
+    doc_ref = db.collection("weights").add({
+        "weight": edited,
+        "date": current_date
+    })
